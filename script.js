d8b08863ae5bee2c22290424bf9d988028d1d9fc
Somesh <someshkjena@gmail.com>
2022-01-05 14:35:13 +0530

    completed

---
+++ script.js
@@ -0,0 +1,30 @@
+daysEl = document.getElementById("days");
+hoursEl = document.getElementById("hours");
+minutesEl = document.getElementById("minutes");
+secondsEl = document.getElementById("seconds");
+
+const newYear = "1 Jan 2023";
+
+function countdown() {
+  const newYearDate = new Date(newYear);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  daysEl.innerHTML = formatTime(days);
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+countdown();
+
+setInterval(countdown, 1000);
